85c21a4047432aa805c08daa9ca4b72ebb6ed816
AMEN <69388528+2331892928@users.noreply.github.com>
2021-12-24 18:27:29 +0800

    Update config_db.php

--- includes/config_db.php
+++ includes/config_db.php
@@ -4,7 +4,7 @@ define('DB_HOST','127.0.0.1');//数据库连接地址，默认：localhost或127
 define('DB_PORT','3306');//数据库端口
 define('DB_USER','');//数据库账号
 define('DB_PASSWD','');//数据库密码
-define('DB_NAME','cshj123');//数据库名称
+define('DB_NAME','');//数据库名称
 define('DB_PRE','ymdl_');//数据库前缀
 define('HT_USER','');//后台账号
 define('HT_PASS','');//后台密码
